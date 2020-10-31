d49a5ff0690780e9945ec0c4e63bfad6629468ec
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-10-31 20:13:25 +0700

    Add files via upload

---
+++ BrakeZone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrakeZone : MonoBehaviour {
+
+	// Use this for initialization
+	public float targetSpeed = 14f;
+
+	float orginalSpeed;
+
+	void OnTriggerEnter(Collider col)
+	{
+
+		if (col.CompareTag ("Player")) {
+			orginalSpeed = col.GetComponent<NavMeshAgent> ().speed;
+			col.GetComponent<NavMeshAgent> ().speed = targetSpeed;
+		}
+		
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+
+		if(col.CompareTag("Player"))
+			col.GetComponent<NavMeshAgent> ().speed = orginalSpeed;
+	}
+}
